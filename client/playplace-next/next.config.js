8875cb92a2dbed95d72ee3c9c05c881513b91725
Im-hass <huiseon4107@gmail.com>
2023-10-25 08:11:38 +0900

    [FIX] 폰트  경고 수정

--- client/playplace-next/next.config.js
+++ client/playplace-next/next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output : 'standalone',
-    basePath: '/pp',
-    compiler: {
-        styledComponents: true,
-    }
-}
+	output: 'standalone',
+	basePath: '/pp',
+	compiler: {
+		styledComponents: true,
+	},
+	// async redirects() {
+	// 	return [
+	// 		{
+	// 			source: '/',
+	// 			destination: '/pp',
+	// 			permanent: true,
+	// 			basePath: false,
+	// 		},
+	// 	];
+	// },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
