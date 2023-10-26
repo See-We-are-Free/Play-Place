46a28f19a7ed52e0be9e84b7e8c659cbc65aa25a
조희라 <bunny7531@naver.com>
2023-10-26 17:41:43 +0900

    [BACKUP] 위도, 경도로 geo-hash 조회 후 주변 geo-hash 가져옴

--- server/src/main/java/kr/co/playplace/controller/radar/RadarController.java
+++ server/src/main/java/kr/co/playplace/controller/radar/RadarController.java
@@ -1,23 +1,24 @@
 package kr.co.playplace.controller.radar;
 
+import kr.co.playplace.common.security.dto.SecurityUserDto;
 import kr.co.playplace.service.radar.RadarService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/radar")
 @RequiredArgsConstructor
 public class RadarController {
-    RadarService radarService;
 
-    @GetMapping("/test")
-    public void findTest(String location) {
-        radarService.findTest(location);
+    private final RadarService radarService;
+
+    @GetMapping
+    public void findAroundUser(@AuthenticationPrincipal SecurityUserDto securityUserDto, @RequestParam Double longitude, @RequestParam Double latitude) {
+        radarService.findAroundUser(longitude, latitude);
     }
 
 }
--- server/src/main/java/kr/co/playplace/service/radar/RadarService.java
+++ server/src/main/java/kr/co/playplace/service/radar/RadarService.java
@@ -1,19 +1,27 @@
 package kr.co.playplace.service.radar;
 
 import ch.hsr.geohash.GeoHash;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class RadarService {
-    public void findTest(String location) {
 
-        GeoHash geoHash = GeoHash.fromGeohashString(location);
+    public void findAroundUser(double longitude, double latitude) {
+
+        redisTemplate.
+
+        GeoHash geoHash = GeoHash.withCharacterPrecision(latitude, longitude, 7);
         GeoHash[] adjHash = geoHash.getAdjacent();
 
+        log.debug("location : {}", geoHash.toBase32());
+
         for(GeoHash adj : adjHash) {
-            log.debug(adj.toBase32());
+            log.debug("adjLocation : {}",adj.toBase32());
         }
     }
 }
