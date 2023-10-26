92da283c7a1104ef1737b576a91bb6c24475ce73
Im-hass <huiseon4107@gmail.com>
2023-10-26 15:20:19 +0900

    [FIX] header props 추가

--- client/playplace-next/src/app/page.tsx
+++ client/playplace-next/src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<LayoutWithHeaderAndTabbar>
+		<LayoutWithHeaderAndTabbar header={<header>헤더</header>}>
 			<h1>PlayPlace Home</h1>
 			<h2>함께 만드는 위치 기반 공유 플레이리스트</h2>
 			<h3>C109</h3>
