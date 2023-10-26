f4bc28498391376b9d24f49fbbb45c78e442660e
wjs_5025@naver.com <wjs_5025@naver.com>
2023-10-26 15:35:53 +0900

    [BUILD] storybook config 추가

--- client/playplace-next/.storybook/main.ts
+++ client/playplace-next/.storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
+const path = require('path');
+
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
@@ -16,6 +18,12 @@ const config: StorybookConfig = {
 	},
 	docs: {
 		autodocs: 'tag',
+	},
+	webpackFinal: async (config) => {
+		if (config.resolve?.alias) {
+			config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+		}
+		return config;
 	},
 };
 export default config;
