6bfd0be8536f0b8302155eb404870f7f4d6f8f45
Im-hass <huiseon4107@gmail.com>
2023-10-26 10:46:17 +0900

    [FIX] Text 관련 style 파일 지움

--- client/playplace-next/src/app/page.tsx
+++ client/playplace-next/src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import TextTest from './../components/molecules/TextTest';
 
 export default function Home() {
 	return (
@@ -9,27 +8,25 @@ export default function Home() {
 			<h1>PlayPlace Home</h1>
 			<h2>함께 만드는 위치 기반 공유 플레이리스트</h2>
 			<h3>C109</h3>
-			<TextTest />
-			<ul>
-				<li>
-					<Link href="/">메인페이지(여기)</Link>
-				</li>
-				<li>
-					<Link href="/login">로그인</Link>
-				</li>
-				<li>
-					<Link href="/signup">회원가입</Link>
-				</li>
-				<li>
-					<Link href="/map">플레이맵</Link>
-				</li>
-				<li>
-					<Link href="/radar">플레이더</Link>
-				</li>
-				<li>
-					<Link href="/search">검색</Link>
-				</li>
-			</ul>
+			<nav>
+				<ul>
+					<li>
+						<Link href="/login">로그인</Link>
+					</li>
+					<li>
+						<Link href="/signup">회원가입</Link>
+					</li>
+					<li>
+						<Link href="/map">플레이맵</Link>
+					</li>
+					<li>
+						<Link href="/radar">플레이더</Link>
+					</li>
+					<li>
+						<Link href="/search">검색</Link>
+					</li>
+				</ul>
+			</nav>
 		</main>
 	);
 }
--- client/playplace-next/src/themes/index.ts
+++
@@ -1,9 +0,0 @@
-import colors from './colors';
-import fontSizes from './fontSizes';
-import space from './space';
-
-export const theme = {
-	space,
-	fontSizes,
-	colors,
-} as const;
