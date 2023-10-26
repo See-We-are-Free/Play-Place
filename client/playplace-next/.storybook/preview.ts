1fc9d8294b7331df14ff82aaa9c9add8539c0dbc
wjs_5025@naver.com <wjs_5025@naver.com>
2023-10-26 13:28:02 +0900

    [BUILD] storybook 라이브러리 추가

---
+++ client/playplace-next/.storybook/preview.ts
@@ -0,0 +1,22 @@
+import type { Preview } from '@storybook/react';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import { GlobalStyles } from '../src/styles/GlobalStyles';
+
+const preview: Preview = {
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export const decorators = [
+	withThemeFromJSXProvider({
+		GlobalStyles,
+	}),
+];
+export default preview;
