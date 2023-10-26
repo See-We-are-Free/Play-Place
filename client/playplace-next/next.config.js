8dc8ff1594389743da9ffb0324eee424332bcd0c
wjs_5025@naver.com <wjs_5025@naver.com>
2023-10-26 17:08:27 +0900

    [BUILD] add webpack svg config

--- client/playplace-next/next.config.js
+++ client/playplace-next/next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
 	compiler: {
 		styledComponents: true,
 	},
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+		return config;
+	},
 	// async redirects() {
 	// 	return [
 	// 		{
