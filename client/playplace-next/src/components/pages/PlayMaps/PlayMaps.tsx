803ed5238758ce1c19deb1e4230df67050c2f22f
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-27 09:18:38 +0900

    [FIX] PlayMaps 주석처리

--- client/playplace-next/src/components/pages/PlayMaps/PlayMaps.tsx
+++ client/playplace-next/src/components/pages/PlayMaps/PlayMaps.tsx
@@ -1,34 +1,34 @@
-import React, { useState, useCallback } from 'react';
-import { nightModeStyles } from './style';
-import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
-import { MapsCenter } from '@/types/maps';
+import React from 'react';
+// import { nightModeStyles } from './style';
+// import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
+// import { MapsCenter } from '@/types/maps';
 
 function PlayMaps() {
-	const [map, setMap] = useState<google.maps.Map | null>(null);
-	const [center, setCenter] = useState<MapsCenter>({
-		lat: 37.5037779,
-		lng: 127.043148,
-	});
+	// const [map, setMap] = useState<google.maps.Map | null>(null);
+	// const [center, setCenter] = useState<MapsCenter>({
+	// 	lat: 37.5037779,
+	// 	lng: 127.043148,
+	// });
 
-	const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS || '';
+	// const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS || '';
 
-	const { isLoaded } = useJsApiLoader({
-		id: 'google-map-script',
-		googleMapsApiKey,
-	});
+	// const { isLoaded } = useJsApiLoader({
+	// 	id: 'google-map-script',
+	// 	googleMapsApiKey,
+	// });
 
-	const onLoad = useCallback(function callback(map: React.SetStateAction<google.maps.Map | null>) {
-		const bounds = new window.google.maps.LatLngBounds(center);
-		if (map !== null) {
-			map.fitBounds(bounds);
-		}
+	// const onLoad = useCallback(function callback(map: React.SetStateAction<google.maps.Map | null>) {
+	// 	const bounds = new window.google.maps.LatLngBounds(center);
+	// 	if (map !== null) {
+	// 		// map.fitBounds(bounds);
+	// 	}
 
-		setMap(map);
-	}, []);
+	// 	setMap(map);
+	// }, []);
 
-	const onUnmount = useCallback(function callback(map) {
-		setMap(null);
-	}, []);
+	// const onUnmount = useCallback(function callback(map) {
+	// 	setMap(null);
+	// }, []);
 
 	return <div>PlayMaps</div>;
 }
