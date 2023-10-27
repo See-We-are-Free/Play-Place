4734079e501cc76932ee1ea0afb6ac0a85ccb2ff
wjs_5025@naver.com <wjs_5025@naver.com>
2023-10-27 17:41:34 +0900

    [FEAT] SongInfo

---
+++ client/playplace-next/src/components/molecules/SongInfo/SongInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SongThumbnail from '@/components/atoms/SongThumbnail/SongThumbnail';
+import SongInfoContainer from './style';
+
+function SongInfo() {
+	return (
+		<SongInfoContainer>
+			<SongThumbnail src="" />
+		</SongInfoContainer>
+	);
+}
+
+export default SongInfo;
