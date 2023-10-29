3115df3d64b8bdfc0aaf460ff1813b3d3a511b74
wjs5025 <wjs_5025@naver.com>
2023-10-29 19:29:39 +0900

    [FEAT] onClick function

--- client/playplace-next/src/components/molecules/PlayBarMenu/PlayBarMenu.tsx
+++ client/playplace-next/src/components/molecules/PlayBarMenu/PlayBarMenu.tsx
@@ -5,20 +5,26 @@ import SkipNext from '@root/public/assets/icons/SkipNext.svg';
 import PlayList from '@root/public/assets/icons/PlayList.svg';
 
 import IconButton from '@/components/atoms/IconButton/IconButton';
+import { useRecoilState } from 'recoil';
+import { playModalState } from '@/recoil/play';
 import PlayBarMenuContainer from './style';
 
 function PlayBarMenu() {
+	const [, setPlayModal] = useRecoilState(playModalState);
 	const handlePrevious = () => {
 		alert('이전 곡');
 	};
+
 	const handlePlay = () => {
 		alert('재생');
 	};
+
 	const handleNext = () => {
 		alert('다음 곡');
 	};
+
 	const handlePlayList = () => {
-		alert('플레이리스트');
+		setPlayModal('playlist');
 	};
 	return (
 		<PlayBarMenuContainer>
