159a3d3a0904d340cd96b374c599f619c4d74533
wjs5025 <wjs_5025@naver.com>
2023-10-29 19:29:53 +0900

    [DESIGN] PlayBar 속성변경

--- client/playplace-next/src/components/organisms/PlayBar/style.ts
+++ client/playplace-next/src/components/organisms/PlayBar/style.ts
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 const PlayBarContainer = styled.div`
+	position: relative;
 	width: 100%;
 	height: 70px;
 	position: fixed;
 	bottom: 70px;
 	display: flex;
 	flex-direction: row;
-	align-items: center;
 	justify-content: space-between;
+	align-items: center;
 	background-color: var(--black-500);
 	padding: 0 10px;
 	border-bottom: 1px solid var(--white-800);
