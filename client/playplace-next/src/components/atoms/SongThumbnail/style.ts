df55d87bc327d6ceeabbbebfe38f147a6ba2c498
wjs5025 <wjs_5025@naver.com>
2023-10-30 01:59:25 +0900

    [DESIGN] radius 추가

--- client/playplace-next/src/components/atoms/SongThumbnail/style.ts
+++ client/playplace-next/src/components/atoms/SongThumbnail/style.ts
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styled from 'styled-components';
 
 const SongThumbnailWrapper = styled(Image)`
+	border-radius: var(--radius-s);
 	width: 100%;
 	height: auto;
 `;
