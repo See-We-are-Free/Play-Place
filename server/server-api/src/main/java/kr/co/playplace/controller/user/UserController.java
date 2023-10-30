216996c1ae9bc4d095164164a807f119c02519ba
홍승준 <ghdgn22@gmail.com>
2023-10-30 10:39:27 +0900

    [FEAT] 회원 중복 검사

--- server/server-api/src/main/java/kr/co/playplace/controller/user/UserController.java
+++ server/server-api/src/main/java/kr/co/playplace/controller/user/UserController.java
@@ -1,7 +1,10 @@
 package kr.co.playplace.controller.user;
 
 import kr.co.playplace.common.ApiResponse;
+import kr.co.playplace.common.exception.BaseException;
+import kr.co.playplace.common.exception.ErrorCode;
 import kr.co.playplace.controller.user.requeset.JoinUserRequest;
+import kr.co.playplace.service.user.UserQueryService;
 import kr.co.playplace.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,9 +21,13 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class UserController {
     private final UserService userService;
+    private final UserQueryService userQueryService;
 
     @PostMapping
     public ApiResponse<Object> register(@RequestBody JoinUserRequest joinUserRequest, HttpServletResponse response) {
+
+        if (userQueryService.findByEmail(joinUserRequest.getEmail()).isPresent())
+            throw new BaseException(ErrorCode.ALREADY_EXIST_USER);
 
         String accessToken = userService.save(joinUserRequest.toJoinUserDto());
 
