dd60d8b8da489b1cf7abc5fa834474218b7c2813
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-31 13:40:34 +0900

    [FEAT] LandMark Icons

--- client/playplace-next/src/components/pages/PlayMaps/PlayMaps.tsx
+++ client/playplace-next/src/components/pages/PlayMaps/PlayMaps.tsx
@@ -11,7 +11,7 @@ function deg2rad(deg: number) {
 }
 
 function CalDistance(lat1: number, lat2: number, lng1: number, lng2: number) {
-	const Earth = 6371;  
+	const Earth = 6371;
 	const dLat = deg2rad(lat2 - lat1);
 	const dLon = deg2rad(lng2 - lng1);
 	const a =
