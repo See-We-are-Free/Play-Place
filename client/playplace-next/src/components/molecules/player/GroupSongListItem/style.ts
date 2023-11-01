21f94160814607778d6253b9977d47afc5bdef49
wjs_5025 <wjs_5025@naver.com>
2023-11-02 02:50:21 +0900

    [FEAT] GroupSongListItem more icon

--- client/playplace-next/src/components/molecules/player/GroupSongListItem/style.ts
+++ client/playplace-next/src/components/molecules/player/GroupSongListItem/style.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const GroupSongListItemContainer = styled.div`
+	position: relative;
 	display: flex;
 	flex-direction: row;
 	gap: 10px;
@@ -9,6 +10,19 @@ const GroupSongListItemContainer = styled.div`
 		display: flex;
 		flex-direction: column;
 		justify-content: space-evenly;
+	}
+
+	#more {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+		right: 0;
+		width: 30px;
+		height: 30px;
+		svg {
+			width: 22px;
+			height: 22px;
+		}
 	}
 `;
 
