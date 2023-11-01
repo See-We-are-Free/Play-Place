c323c2936d51ca05ebc3535db2bfd7b4c8fc1317
byunyc124 <byunyc124@naver.com>
2023-11-01 14:18:13 +0900

    [FEAT] 재생목록 조회 시 그룹 재생목록 조회 추가

--- server/server-api/src/main/java/kr/co/playplace/repository/landmark/UserLandmarkGroupRepository.java
+++ server/server-api/src/main/java/kr/co/playplace/repository/landmark/UserLandmarkGroupRepository.java
@@ -4,6 +4,7 @@ import kr.co.playplace.entity.user.UserLandmarkGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface UserLandmarkGroupRepository extends JpaRepository<UserLandmarkG
     void deleteUserLandmarkGroupByUserIdAndLandmarkId(Long userId, Long landmarkId);
 
     int countByUserId(Long userId);
+
+    List<UserLandmarkGroup> findByUser_Id(long userId);
 }
