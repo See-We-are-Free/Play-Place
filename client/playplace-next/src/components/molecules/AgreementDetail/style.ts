79f641c6218774ce51cac92e1149e8b3953486ad
Im-hass <huiseon4107@gmail.com>
2023-11-01 17:45:51 +0900

    [DESIGN] 약관 높이 설정

--- client/playplace-next/src/components/molecules/AgreementDetail/style.ts
+++ client/playplace-next/src/components/molecules/AgreementDetail/style.ts
@@ -29,11 +29,16 @@ export const Title = styled.h2`
 
 export const Content = styled.div`
 	width: 100%;
+	height: calc(100vh - 130px);
 	border: 1px solid var(--black-400);
 	border-radius: 5px;
 	padding: 10px;
 	background-color: var(--black-600);
 	font-size: 12px;
+	overflow: scroll;
+	&::-webkit-scrollbar {
+		display: none; /* Chrome, Safari, Opera*/
+	}
 
 	pre {
 		text-wrap: pretty;
