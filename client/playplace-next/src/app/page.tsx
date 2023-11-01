008071074a59e28cd58360ec69980e83fa9efb01
Kim SeoungHyeon <kim0221_@naver.com>
2023-11-01 17:36:44 +0900

    [FEAT] HomeAlbum 컴퍼넌트 리스트 기능 추가

--- client/playplace-next/src/app/page.tsx
+++ client/playplace-next/src/app/page.tsx
@@ -6,13 +6,16 @@ import HomeAlbum from '@/components/molecules/HomeAlbum/HomeAlbum';
 import LandMark from '@root/public/assets/icons/LandMark.svg';
 import LayoutWithHeader from '@/components/templates/layout/LayoutWithHeader/LayoutWithHeader';
 import { HeaderStyles } from '@/types/styles.d';
-import HypeBoy from '@root/public/assets/images/hypeBoy.jpg';
+// import HypeBoy from '@root/public/assets/images/hypeBoy.jpg';
 import MenuIcon from '@root/public/assets/icons/Menu.svg';
 import Link from 'next/link';
+import { SONG_DATA } from '@/types/home.d';
+import { useState } from 'react';
 import ContentLayout from '../components/templates/layout/ContentLayout/ContentLayout';
 
 export default function Home() {
 	const name = '임하스';
+	const [songData] = useState(SONG_DATA);
 	const header = (
 		<Header $headerType={HeaderStyles.home} location="장덕동">
 			<button type="button" onClick={() => console.log('클릭')}>
@@ -25,7 +28,7 @@ export default function Home() {
 		<LayoutWithHeader header={header}>
 			<ContentLayout $margin="0 0 140px">
 				<LandMark />
-				<HomeAlbum artist="NewJeans" imgSrc={HypeBoy} title="HypeBoy" />
+				<HomeAlbum locationSongList={songData} />
 				<HomeSubtitle colorSubtitle="이번주" normalSubtitle="가장 많이 재생된 음악" />
 				<HomeSubtitle colorSubtitle={`${name}님 근처`} normalSubtitle="사람들의 음악" />
 				<h1>PlayPlace Home</h1>
---
+++ client/playplace-next/src/types/home.d.ts
@@ -0,0 +1,45 @@
+interface LocationSongItem {
+	title: string;
+	artist: string;
+	albumImg: string;
+}
+
+export const SONG_DATA: LocationSongItem[] = [
+	{
+		title: 'HypeBoy',
+		artist: 'NewJeans',
+		albumImg: 'test',
+	},
+	{
+		title: 'HypeBoy',
+		artist: 'NewJeans',
+		albumImg: 'test',
+	},
+	{
+		title: 'HypeBoy',
+		artist: 'NewJeans',
+		albumImg: 'test',
+	},
+	{
+		title: 'HypeBoy',
+		artist: 'NewJeans',
+		albumImg: 'test',
+	},
+	{
+		title: 'HypeBoy',
+		artist: 'NewJeans',
+		albumImg: 'test',
+	},
+	{
+		title: 'HypeBoy',
+		artist: 'NewJeans',
+		albumImg: 'test',
+	},
+	{
+		title: 'HypeBoy',
+		artist: 'NewJeans',
+		albumImg: 'test',
+	},
+];
+
+export default LocationSongItem;
