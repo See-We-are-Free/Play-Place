23bb4665c9381edd1bbcf19973d1a1e5ce9b378b
wjs_5025 <wjs_5025@naver.com>
2023-11-03 01:57:01 +0900

    [FEAT] api 연결

--- client/playplace-next/src/components/molecules/search/SearchItems/SearchItems.tsx
+++ client/playplace-next/src/components/molecules/search/SearchItems/SearchItems.tsx
@@ -4,7 +4,9 @@ import Text from '@/components/atoms/Text/Text';
 import { SearchSong, Song } from '@/types/songs';
 import Play from '@root/public/assets/icons/Play.svg';
 import { useRecoilState } from 'recoil';
-import { nowPlaySongState, playbackState } from '@/recoil/play';
+import { isNowPlayState, nowPlaySongState } from '@/recoil/play';
+import { saveSongToPlaylistApi } from '@/utils/api/songs';
+import useFetchPlaylist from '@/hooks/player/useFetchPlaylist';
 import SearchItemsContainer, { SearchItemsButton, SearchItemsContent, SearchItemsSongInfo } from './style';
 
 interface ISearchItemsProps {
@@ -12,24 +14,38 @@ interface ISearchItemsProps {
 }
 
 function SearchItems(props: ISearchItemsProps) {
+	const { fetchData } = useFetchPlaylist();
 	const { searchItem } = props;
 	const artist = searchItem.snippet.channelTitle.replace(' - Topic', '');
+	const [, setIsNowPlay] = useRecoilState(isNowPlayState);
 	const [, setNowPlaySong] = useRecoilState(nowPlaySongState);
-	const [playback] = useRecoilState(playbackState);
 
-	// playback이 업데이트될 때 실행
+	// 검색 결과 노래 재생시
 	const handlePlay = async () => {
 		const song: Song = {
 			title: searchItem.snippet.title,
 			youtubeId: searchItem.id.videoId,
 			albumImg: searchItem.snippet.thumbnails.default.url,
 			artist,
-			playTime: -1, // 초기값 설정
+			playTime: -1, // 이 값을 바꾸고싶어.
 			songId: -1,
 		};
 
 		setNowPlaySong(song);
-		console.log(playback.getDuration());
+		setIsNowPlay(true);
+
+		try {
+			const response = await saveSongToPlaylistApi(song);
+			if (response.status === 200) {
+				setNowPlaySong((state) => {
+					if (state) return { playlistSongId: response.data.playListSongId, ...state };
+					return state;
+				});
+				fetchData();
+			}
+		} catch (error) {
+			console.error(error);
+		}
 	};
 
 	return (
