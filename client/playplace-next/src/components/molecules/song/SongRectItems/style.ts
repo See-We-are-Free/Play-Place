c190c08c97f5959b51d5509f77b47f8904def60b
김승현 <kim0221_@naver.com>
2023-11-02 12:09:38 +0900

    [FEAT] Home 정사각형앨범Items 컴포넌트 생성

---
+++ client/playplace-next/src/components/molecules/song/SongRectItems/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SongRectItemsContainer = styled.div`
+	border-radius: var(--radius-s);
+	margin: 10px 0;
+`;
+
+
+export default SongRectItemsContainer;
