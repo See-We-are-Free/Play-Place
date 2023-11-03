2e949bbb0363ad0416e9bcf7cea624c5a2aa98a3
wjs5025 <wjs_5025@naver.com>
2023-11-03 23:50:04 +0900

    [FEAT] 챗봇 관련 컴포넌트 구현

---
+++ client/playplace-next/src/components/organisms/chatbot/BottomMenu/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const BottomMenuContainer = styled.div`
+	width: 100%;
+	height: 70px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+
+	#camera-icon {
+		background: var(--primary-grandiant-main);
+		border-radius: var(--radius-l);
+		width: 50px;
+		height: 50px;
+		padding: 10px;
+	}
+`;
+
+export default BottomMenuContainer;
