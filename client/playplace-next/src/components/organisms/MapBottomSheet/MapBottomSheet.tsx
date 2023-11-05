afe738d216a9655aad44608124cf2cb6a29d6a43
Kim SeoungHyeon <kim0221_@naver.com>
2023-11-06 00:55:59 +0900

    [FEAT] MapBottomSheet 컴포넌트 생성

---
+++ client/playplace-next/src/components/organisms/MapBottomSheet/MapBottomSheet.tsx
@@ -0,0 +1,48 @@
+import MapBottomInfo from '@/components/molecules/MapBottomInfo/MapBottomInfo';
+import ContentLayout from '@/components/templates/layout/ContentLayout/ContentLayout';
+import { Song } from '@/types/songs';
+import React, { useState } from 'react';
+import SearchList from '@/components/molecules/search/SearchList/SearchList';
+import { ContentLayoutSizes } from '@/types/styles.d';
+// import SearchSongs from '@/components/pages/SearchSongs/SearchSongs';
+import MapBottomSheetHR, { MapBottomResult, MapBottomResultText } from './style';
+import LandMarkSearch from '../LandMarkSearch/LandMarkSearch';
+
+interface IMapBottomSheetProps {
+	landMarkTitle: string;
+	landMarkList: Song[];
+	isDistance: boolean;
+}
+
+function MapBottomSheet(props: IMapBottomSheetProps) {
+	const { landMarkTitle, landMarkList, isDistance } = props;
+	const [open, setOpen] = useState<boolean>(false);
+
+	if (open === true) {
+		return <LandMarkSearch setOpen={setOpen} landMarkTitle={landMarkTitle} />;
+	}
+	return (
+		<ContentLayout size={ContentLayoutSizes.md}>
+			<ContentLayout $margin="20px 0">
+				<MapBottomInfo
+					songVolume={landMarkList.length}
+					landMarkTitle={landMarkTitle}
+					setOpen={setOpen}
+					isDistance={isDistance}
+				/>
+			</ContentLayout>
+			<MapBottomSheetHR />
+			<ContentLayout $margin="20px 0px 40px 0px">
+				<MapBottomResult>
+					{landMarkList.length ? (
+						<SearchList searchList={landMarkList} />
+					) : (
+						<MapBottomResultText>랜드마크 노래를 추가해보세요!</MapBottomResultText>
+					)}
+				</MapBottomResult>
+			</ContentLayout>
+		</ContentLayout>
+	);
+}
+
+export default MapBottomSheet;
