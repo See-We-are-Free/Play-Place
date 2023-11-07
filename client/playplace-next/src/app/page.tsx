52d49079fc9e4d4c6042ed0410c3f61db7006353
Kim SeoungHyeon <kim0221_@naver.com>
2023-11-08 02:55:17 +0900

    [FEAT] 홈페이지 Android  위치권한 요청

--- client/playplace-next/src/app/page.tsx
+++ client/playplace-next/src/app/page.tsx
@@ -6,9 +6,16 @@ import { HeaderStyles } from '@/types/styles.d';
 import MenuIcon from '@root/public/assets/icons/Menu.svg';
 import Home from '@/components/pages/Home/Home';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function HomePage() {
 	const router = useRouter();
+
+	useEffect(() => {
+		if (window.AndMap) {
+			window.AndMap.successLocate();
+		}
+	}, []);
 
 	const header = (
 		<Header $headerType={HeaderStyles.home} location="장덕동">
