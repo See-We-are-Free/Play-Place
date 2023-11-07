93cf859d15a8b28f8de2b4c15c2418bb2571244c
byunyc124 <byunyc124@naver.com>
2023-11-07 14:33:23 +0900

    [BUILD] vision 설정 제거

--- server/server-api/src/main/java/kr/co/playplace/controller/chatbot/ChatbotController.java
+++ server/server-api/src/main/java/kr/co/playplace/controller/chatbot/ChatbotController.java
@@ -1,31 +1,31 @@
-package kr.co.playplace.controller.chatbot;
-
-
-import kr.co.playplace.common.ApiResponse;
-import kr.co.playplace.service.chatbot.OpenAIService;
-import kr.co.playplace.service.chatbot.VisionService;
-import kr.co.playplace.service.chatbot.dto.ChatbotDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/v1/chatbots")
-public class ChatbotController {
-    private final VisionService visionService;
-    private final OpenAIService openAIService;
-
-    @PostMapping
-    public ApiResponse<Object> getLable(@RequestParam("img") MultipartFile img) {
-        String imgLabels = visionService.detectLabelFromImage(img);
-        ChatbotDto result = openAIService.getResponse(imgLabels);
-        return ApiResponse.ok(result);
-    }
-
-    @PostMapping("/test")
-    public ApiResponse<Object> getTEST(@RequestParam("img") String imgLabels) {
-        String result = String.valueOf(openAIService.getResponse(imgLabels));
-        return ApiResponse.ok(result);
-    }
-}
+//package kr.co.playplace.controller.chatbot;
+//
+//
+//import kr.co.playplace.common.ApiResponse;
+//import kr.co.playplace.service.chatbot.OpenAIService;
+//import kr.co.playplace.service.chatbot.VisionService;
+//import kr.co.playplace.service.chatbot.dto.ChatbotDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/api/v1/chatbots")
+//public class ChatbotController {
+//    private final VisionService visionService;
+//    private final OpenAIService openAIService;
+//
+//    @PostMapping
+//    public ApiResponse<Object> getLable(@RequestParam("img") MultipartFile img) {
+//        String imgLabels = visionService.detectLabelFromImage(img);
+//        ChatbotDto result = openAIService.getResponse(imgLabels);
+//        return ApiResponse.ok(result);
+//    }
+//
+//    @PostMapping("/test")
+//    public ApiResponse<Object> getTEST(@RequestParam("img") String imgLabels) {
+//        String result = String.valueOf(openAIService.getResponse(imgLabels));
+//        return ApiResponse.ok(result);
+//    }
+//}
--- server/server-api/src/main/java/kr/co/playplace/service/chatbot/VisionService.java
+++ server/server-api/src/main/java/kr/co/playplace/service/chatbot/VisionService.java
@@ -1,47 +1,47 @@
-package kr.co.playplace.service.chatbot;
-
-import com.google.cloud.spring.vision.CloudVisionTemplate;
-import com.google.cloud.vision.v1.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-
-
-@Transactional
-@Service
-public class VisionService{
-
-    @Autowired
-    private CloudVisionTemplate cloudVisionTemplate;
-
-    @Autowired
-    private ResourceLoader resourceLoader;
-
-    public String detectLabelFromImage(MultipartFile file) {
-
-        AnnotateImageResponse response =
-                cloudVisionTemplate.analyzeImage(
-                        file.getResource(), Feature.Type.LABEL_DETECTION);
-
-        List<EntityAnnotation> labels = response.getLabelAnnotationsList();
-
-        List<String> descriptions = new ArrayList<>();
-        for (EntityAnnotation label : labels) {
-            descriptions.add(label.getDescription());
-        }
-        return descriptions.toString();
-    }
-
-}
+//package kr.co.playplace.service.chatbot;
+//
+//import com.google.cloud.spring.vision.CloudVisionTemplate;
+//import com.google.cloud.vision.v1.*;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.ResourceLoader;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import javax.imageio.ImageIO;
+//import java.awt.*;
+//import java.awt.image.BufferedImage;
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//import java.nio.file.Path;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//@Transactional
+//@Service
+//public class VisionService{
+//
+//    @Autowired
+//    private CloudVisionTemplate cloudVisionTemplate;
+//
+//    @Autowired
+//    private ResourceLoader resourceLoader;
+//
+//    public String detectLabelFromImage(MultipartFile file) {
+//
+//        AnnotateImageResponse response =
+//                cloudVisionTemplate.analyzeImage(
+//                        file.getResource(), Feature.Type.LABEL_DETECTION);
+//
+//        List<EntityAnnotation> labels = response.getLabelAnnotationsList();
+//
+//        List<String> descriptions = new ArrayList<>();
+//        for (EntityAnnotation label : labels) {
+//            descriptions.add(label.getDescription());
+//        }
+//        return descriptions.toString();
+//    }
+//
+//}
