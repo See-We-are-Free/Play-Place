e80cd7427ae7e3980291afab820713691a6d7da1
Kim SeoungHyeon <kim0221_@naver.com>
2023-11-10 11:14:06 +0900

    [FEAT] Mypage Like 기능 설정

---
+++ client/playplace-next/src/components/organisms/mypage/MypageLike/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const MypageLikeItemsContainer = styled.div`
+	background-color: var(--black-500);
+	border-radius: var(--radius-s);
+	padding: 10px;
+`;
+
+export default MypageLikeItemsContainer;
