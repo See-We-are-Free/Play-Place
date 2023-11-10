52445ec26974d47e80932c36789a8c8422276fa0
byunyc124 <byunyc124@naver.com>
2023-11-10 13:56:44 +0900

    [MODIFY] rt만료시 401 코드 반환

--- server/server-api/src/main/java/kr/co/playplace/controller/auth/AuthController.java
+++ server/server-api/src/main/java/kr/co/playplace/controller/auth/AuthController.java
@@ -5,6 +5,7 @@ import kr.co.playplace.common.util.SecurityUtils;
 import kr.co.playplace.service.user.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,7 +40,7 @@ public class AuthController {
             return ResponseEntity.ok(StatusResponseDto.addStatus(200));
         }
 
-        return ResponseEntity.badRequest().body(StatusResponseDto.addStatus(400));
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
 }
