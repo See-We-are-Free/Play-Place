c37b8e1b467a3cbb5a2c91d7de8daf663a8578f4
Kim SeoungHyeon <kim0221_@naver.com>
2023-11-10 11:13:32 +0900

    [FEAT] Mypage Setting 기능 설정

---
+++ client/playplace-next/src/components/molecules/MypageToggle/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const MyPageToggleContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: 10px;
+`;
+
+export default MyPageToggleContainer;
---
+++ client/playplace-next/src/components/organisms/mypage/MypageSetting/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const MypageSettingContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding: 10px;
+`;
+
+export const MypagetSettingText = styled.div`
+	padding: 10px;
+`;
+
+export default MypageSettingContainer;
