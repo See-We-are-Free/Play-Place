6863f7989a9a33c3778f05a2284ca006abff21c7
조희라 <bunny7531@naver.com>
2023-11-13 16:51:12 +0900

    [FEAT] 추천 곡 더미데이터

--- server/server-api/src/main/java/kr/co/playplace/common/util/location/DataLoader.java
+++ server/server-api/src/main/java/kr/co/playplace/common/util/location/DataLoader.java
@@ -3,10 +3,21 @@ package kr.co.playplace.common.util.location;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import kr.co.playplace.entity.Timezone;
+import kr.co.playplace.entity.Weather;
 import kr.co.playplace.entity.location.State;
+import kr.co.playplace.entity.location.Village;
+import kr.co.playplace.entity.song.Song;
+import kr.co.playplace.entity.song.SongHistory;
+import kr.co.playplace.entity.user.Users;
 import kr.co.playplace.repository.location.CityJDBCRepository;
 import kr.co.playplace.repository.location.StateJDBCRepository;
 import kr.co.playplace.repository.location.VillageJDBCRepository;
+import kr.co.playplace.repository.location.VillageRepository;
+import kr.co.playplace.repository.song.SongHistoryRepository;
+import kr.co.playplace.repository.song.SongRepository;
+import kr.co.playplace.repository.user.UserRepository;
+import kr.co.playplace.service.song.SongService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -19,7 +30,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Component
@@ -29,6 +43,9 @@ public class DataLoader {
     private final StateJDBCRepository stateJDBCRepository;
     private final CityJDBCRepository cityJDBCRepository;
     private final VillageJDBCRepository villageJDBCRepository;
+    private final SongRepository songRepository;
+    private final SongHistoryRepository songHistoryRepository;
+    private final SongService songService;
 
     @Bean
     public CommandLineRunner stateLoad() {
@@ -130,5 +147,50 @@ public class DataLoader {
                 .build();
 
         return csvToBean.parse();
+    }
+
+    public void createRandomSongHistory(Users user, Village village) {
+        Random rand = new Random();
+
+        for(int i = 0; i < 10; i++) {
+            long songId = 1 + rand.nextInt(39);
+            int weatherType = rand.nextInt(4);
+            int timeZoneType = rand.nextInt(4);
+
+            Optional<Song> song = songRepository.findById(songId);
+
+            Weather weather = Weather.values()[weatherType];
+            Timezone timezone = Timezone.values()[timeZoneType];
+
+            SongHistory songHistory = SongHistory.builder()
+                    .user(user)
+                    .song(song.get())
+                    .village(village)
+                    .weather(weather)
+                    .timezone(timezone)
+                    .build();
+
+            songHistoryRepository.save(songHistory);
+        }
+    }
+
+    @Bean
+    public CommandLineRunner SongHistoryDataLoad(UserRepository userRepository, VillageRepository villageRepository) {
+        return args -> {
+            long count = userRepository.count();
+            if(count == 0) {
+                Users user = new Users("dump", 1, "playplace@gmail.com", 0, 0, 0, "ROLE_ADMIN" );
+                userRepository.save(user);
+
+                for(int i = 1; i <= 3586; i++) {
+                    Optional<Village> village = villageRepository.findById(i);
+                    createRandomSongHistory(user, village.get());
+                }
+
+                songService.getAreaStatistics();
+                songService.getTimezoneStatistics();
+                songService.getWeatherStatistics();
+            }
+        };
     }
 }
