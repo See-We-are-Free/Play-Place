71ac9246348a68a64719083f17d3222598bae8af
임희선 <gmltjs4107@gmail.com>
2023-11-13 11:34:05 +0900

    [REFACTOR] 레이더 설명 추가, 위치 갱신 추가

--- client/playplace-next/src/components/organisms/radar/RadarShareOff/RadarShareOff.tsx
+++ client/playplace-next/src/components/organisms/radar/RadarShareOff/RadarShareOff.tsx
@@ -6,6 +6,7 @@ function RadarShareOff() {
 		<TextContainer>
 			<Text text="내 주변 음악을 보려면" fontSize={16} />
 			<Text text="오른쪽 상단의 공유하기를 활성화 해주세요!" fontSize={16} />
+			<Text text="공유하기가 꺼져있을 땐, 내 음악이 공유되지 않아요" fontSize={12} color="gray" />
 		</TextContainer>
 	);
 }
