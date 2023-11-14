08d4ab27145e704e6a86d6e39f7c4be551ba9081
byunyc124 <byunyc124@naver.com>
2023-11-14 13:19:58 +0900

    [MODIFY] 내 공유재생목록에서 곡이 전부 삭제되면 그룹을 삭제

--- server/server-api/src/main/java/kr/co/playplace/service/playlist/PlaylistService.java
+++ server/server-api/src/main/java/kr/co/playplace/service/playlist/PlaylistService.java
@@ -1,5 +1,6 @@
 package kr.co.playplace.service.playlist;
 
+import kr.co.playplace.entity.user.UserLandmarkSong;
 import kr.co.playplace.repository.landmark.UserLandmarkGroupRepository;
 import kr.co.playplace.repository.landmark.UserLandmarkSongRepository;
 import kr.co.playplace.repository.user.UserSongRepository;
@@ -7,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -20,9 +24,16 @@ public class PlaylistService {
 
     public void deleteMySong(boolean isLandmark, long songId){
         if(isLandmark){ // 사용자 랜드마크 곡에서 삭제
+            Optional<UserLandmarkSong> userLandmarkSong = userLandmarkSongRepository.findById(songId);
+            Long userLandmarkGroupId = userLandmarkSong.get().getUserlandmarkGroup().getId();
+
             userLandmarkSongRepository.deleteById(songId);
 
-            // TODO: group에서 song 모두 사라지면 그룹 자체를 삭제
+            // group에서 song 모두 사라지면 그룹 자체를 삭제
+            List<UserLandmarkSong> userLandmarkSongs = userLandmarkSongRepository.findAllByUserlandmarkGroupId(userLandmarkGroupId);
+            if (userLandmarkSongs.isEmpty()){
+                userLandmarkGroupRepository.deleteById(userLandmarkGroupId);
+            }
 
         }else{ // 사용자 곡에서 삭제
             userSongRepository.deleteById(songId);
