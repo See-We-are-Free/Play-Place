fb3e90c2f52bf35c6faeefbb3409377d78313bd9
wjs5025 <wjs_5025@naver.com>
2023-11-15 01:56:14 +0900

    [FEAT] 문의하기 주소 추가

--- client/playplace-landing/js/main.js
+++ client/playplace-landing/js/main.js
@@ -3,5 +3,5 @@ const handleDownload = () => {
 }
 
 const handleInquiry = () => {
-  
+  window.open("https://docs.google.com/forms/d/e/1FAIpQLSd9PbONbINsAKlA0O2B1nW4PK2ucX1PEzGlIEpwxkd7NPHDuQ/viewform?pli=1")
 }
