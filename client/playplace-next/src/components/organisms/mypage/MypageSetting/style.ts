96c7af80d45c815db3b0e840f693a1f46f416d08
임희선 <gmltjs4107@gmail.com>
2023-11-14 11:11:43 +0900

    [DESIGN] 푸시 알림 수신 동의 토글 제거

--- client/playplace-next/src/components/organisms/mypage/MypageSetting/style.ts
+++ client/playplace-next/src/components/organisms/mypage/MypageSetting/style.ts
@@ -6,8 +6,19 @@ const MypageSettingContainer = styled.div`
 	padding: 10px;
 `;
 
+export const Line = styled.div`
+	width: 100%;
+	height: 0.3px;
+	background-color: var(--white-800);
+	margin: 10px 0;
+`;
+
 export const MypagetSettingText = styled.div`
 	padding: 10px;
+
+	& > p {
+		color: var(--white-600);
+	}
 `;
 
 export default MypageSettingContainer;
