c106975b5702908cb282e04ae0eca43c0c6aaaa3
임희선 <gmltjs4107@gmail.com>
2023-11-14 21:54:31 +0900

    [FEAT] 랜딩 페이지 구현

--- client/playplace-landing/js/main.js
+++ client/playplace-landing/js/main.js
@@ -1,3 +1,7 @@
 const handleDownload = () => {
-  console.log('아하');
+  window.open('https://drive.google.com/file/d/1cXRgWM2Q7jMxFYZZ2BbI2WyFg_yqbr7T/view');
+}
+
+const handleInquiry = () => {
+  
 }
