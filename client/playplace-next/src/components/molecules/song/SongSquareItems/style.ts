c1ba75a646758276582bf20724d40be634328487
임희선 <gmltjs4107@gmail.com>
2023-11-15 16:20:40 +0900

    [DESIGN] 메인 화면 앨범 크기 조정

--- client/playplace-next/src/components/molecules/song/SongSquareItems/style.ts
+++ client/playplace-next/src/components/molecules/song/SongSquareItems/style.ts
@@ -5,6 +5,22 @@ const SongSquareItemsContainer = styled.div`
 	margin: 10px 0;
 	padding: 0px;
 	width: 130px;
+
+	& > div {
+		width: 130px;
+		height: 130px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		overflow: hidden;
+		border-radius: 20px;
+
+		& > img {
+			width: 100%;
+			height: auto;
+			transform: scale(1.8);
+		}
+	}
 `;
 
 export default SongSquareItemsContainer;
